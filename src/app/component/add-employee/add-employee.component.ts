393b8a9aaca100e49c9d5b79c3f222733d477fcc
sb58142 <santoshmcaau@gmail.com>
2020-05-11 17:26:04 +0530

    Add employee validation, rest call configure

--- src/app/component/add-employee/add-employee.component.ts
+++ src/app/component/add-employee/add-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Form, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/service/user.service';
+import { EmployeeInformationDto } from 'src/app/model/employeeInformationDto';
 
 @Component({
   selector: 'app-add-employee',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor() { }
+  addNewEmpForm: FormGroup;
+  submitted:boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this._buildAddEmpForm();
+  }
+
+  _buildAddEmpForm(){
+    this.addNewEmpForm = this.fb.group({
+      empId: ['', Validators.required],
+      contactNumber: ['', Validators.required],
+      email: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      location: ['', Validators.required],
+      level: ['', Validators.required],
+      designationRole: ['', Validators.required],
+      projectJoinDate: ['', Validators.required],
+      projectReleaseDate: ['', Validators.required],
+    })
+  }
+
+  get f() { return this.addNewEmpForm.controls; }
+
+  onSubmit(){
+    console.log(this.addNewEmpForm.value);
+    this.submitted = true;
+    const formData = this.addNewEmpForm.value;
+    if (this.addNewEmpForm.invalid) {
+      return;
+    }
+    this.userService.addemployee(formData).subscribe(data=>{
+      console.log("Employee Add call done."+data);
+    })
+
   }
 
 }
